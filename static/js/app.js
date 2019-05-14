1d894bc7fe301972e36ea4abc717129ccc515e90
Sofiane Benmesbah <vooveen@gmail.com>
2019-05-14 18:40:39 +0100

    add function to control add button

--- static/js/app.js
+++ static/js/app.js
@@ -14,23 +14,22 @@ var dataController = (function() {
 
 // Controller to link between UI and Budget controllers
 var  controller = (function(UICtrl, dataCtrl) {
-    // Add button fuctionality
-    var addButton = document.querySelector('.add__btn');
-    addButton.addEventListener('click', function() {
+    var ctrlAddButton = function() {
         // TODO LIST
         // 1. Get the data from fieds
         // 2. Add the data to dataController
         // 3. Display data in Incomes or Expenses
         // 4. Calculate the Budget
         // 5. Update the Budget
-    });
+    };
+    // Add button fuctionality
+    var addButton = document.querySelector('.add__btn');
+    addButton.addEventListener('click', ctrlAddButton);
 
     // When taping enter to retrieve data from field
     document.addEventListener('keypress', function(event) {
         if(event.charCode === 13 || event.which === 13 ) {
-            console.log(event);
-            // TODO LIST
-            // Same as AddButton todo list
+            ctrlAddButton();
         }
     });
 })(UIController, dataController);
